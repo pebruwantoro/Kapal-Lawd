e65e13eb1cda1c5af419f0e42b414f22556670a0
Romi Fadhurrohman Nabil <70937653+Ragnarom-kun@users.noreply.github.com>
2024-09-19 09:11:05 +0700

    feat: add AVKit feature with sound

---
+++ Kapal-Lawd/Home/View Model/AudioPlayerViewModel.swift
@@ -0,0 +1,46 @@
+//
+//  AudioPlayerViewModel.swift
+//  Kapal-Lawd
+//
+//  Created by Romi Fadhurohman Nabil on 19/09/24.
+//
+
+import Foundation
+import AVKit
+import Combine
+
+class AudioPlayerViewModel: ObservableObject {
+    private var player: AVPlayer?
+    private var playerItem: AVPlayerItem?
+    @Published var isPlaying = false
+    @Published var currentSongTitle: String?
+
+    func startPlayback(songTitle: String) {
+        guard let url = Bundle.main.url(forResource: songTitle, withExtension: "mp3") else {
+            print("Audio file not found")
+            return
+        }
+        let playerItem = AVPlayerItem(url: url)
+        self.playerItem = playerItem
+        self.player = AVPlayer(playerItem: playerItem)
+        self.player?.play()
+        self.isPlaying = true
+        self.currentSongTitle = songTitle
+    }
+
+    func pausePlayback() {
+        self.player?.pause()
+        self.isPlaying = false
+    }
+
+    func resumePlayback() {
+        self.player?.play()
+        self.isPlaying = true
+    }
+
+    func stopPlayback() {
+        self.player?.pause()
+        self.player?.seek(to: CMTime.zero)
+        self.isPlaying = false
+    }
+}
