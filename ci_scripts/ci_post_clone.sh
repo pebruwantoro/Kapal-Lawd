ffe6ee97554de71abd1ed4a6b07b9919ced656bb
pebruwantoro <pebruwantoro.doni@gmail.com>
2024-09-30 21:56:14 +0700

    fixing

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -15,19 +15,41 @@ cd ..
 echo "Generating Xcode project..."
 xcodegen
 
+echo "Check file on Kapal-Lawd.xcodeproj"
 ls Kapal-Lawd.xcodeproj
 
 mkdir Kapal-Lawd.xcodeproj/project.xcworkspace
 
+echo "Check file on project.xcworkspace"
 ls Kapal-Lawd.xcodeproj/project.xcworkspace 
 
-mkdir Kapal-Lawd.xcodeproj/project.xcworkspace/xcshareddata
+# mkdir Kapal-Lawd.xcodeproj/project.xcworkspace/xcshareddata
 
+echo "Check file on xcshareddata"
 ls Kapal-Lawd.xcodeproj/project.xcworkspace/xcshareddata
 
 mkdir Kapal-Lawd.xcodeproj/project.xcworkspace/xcshareddata/swiftpm
 
 touch Kapal-Lawd.xcodeproj/project.xcworkspace/xcshareddata/swiftpm/Package.resolved
+
+echo "Creating Package.resolved..."
+cat <<EOL > project.xcworkspace/xcshareddata/swiftpm/Package.resolved
+{
+  "originHash" : "fc9f608b2604b47e8a4e8f6d34706a07df80b7a48044b46fc831990fd3c8aece",
+  "pins" : [
+    {
+      "identity" : "sdbeaconscanner",
+      "kind" : "remoteSourceControl",
+      "location" : "https://github.com/SagarSDagdu/SDBeaconScanner.git",
+      "state" : {
+        "revision" : "1b9ce5a1ba30691f7c4d404e6efd8c9ed02c230e",
+        "version" : "0.0.2"
+      }
+    }
+  ],
+  "version" : 3
+}
+EOL
 
 # Resolve package dependencies to generate Package.resolved
 echo "Resolving package dependencies..."
