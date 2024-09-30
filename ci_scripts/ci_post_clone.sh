d069b976f0f029953f77320a382d7d9b0b90e518
pebruwantoro <pebruwantoro.doni@gmail.com>
2024-09-30 18:02:44 +0700

    add-step-on-xcode-cloud

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -3,20 +3,13 @@
 brew update
 
 # Install Xcodegen
-brew install xcodegen
+if ! command -v xcodegen &> /dev/null
+then
+    brew install xcodegen
+fi
 
 # Go to Project Directory
 cd ..
 
 # Generate the Xcode project
 xcodegen generate
-
-# mkdir -p Kapal-Lawd.xcodeproj/project.xcworkspace/xcshareddata/swiftpm
-
-# ls -la
-# ls $(pwd)/Kapal-Lawd.xcodeproj/project.xcworkspace/xcshareddata/swiftpm
-# cat $(pwd)/Kapal-Lawd.xcodeproj/project.xcworkspace/xcshareddata/swiftpm/Package.resolved
-
-
-# # Resolve Swift package dependencies
-# xcodebuild -project Kapal-Lawd.xcodeproj -scheme Kapal-Lawd -resolvePackageDependencies -verbose
