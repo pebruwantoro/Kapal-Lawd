4f0cc2630011d75575bfb7a912c9d1bb46824e19
pebruwantoro <pebruwantoro.doni@gmail.com>
2024-09-30 21:27:46 +0700

    add-step

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -14,3 +14,9 @@ echo "Generating Xcode project..."
 xcodegen
 
 # Any other post-clone setup tasks can be added here
+# Navigate to the project directory
+cd Kapal-Lawd.xcodeproj
+
+# Create the xcshareddata/swiftpm directory if it doesn't exist
+mkdir -p project.xcworkspace/xcshareddata/swiftpm
+
