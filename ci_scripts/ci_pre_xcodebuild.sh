57309d3f1f44b6bd6f4ac7bc88963c4262023486
pebruwantoro <pebruwantoro.doni@gmail.com>
2024-09-30 21:04:39 +0700

    add-scripts

---
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Check if the project was generated successfully
+if [ ! -d "Kapal-Lawd.xcodeproj" ]; then
+    echo "Failed to generate Xcode project."
+    exit 1
+fi
+
+# Resolve Swift Package Manager dependencies
+xcodebuild -resolvePackageDependencies -project ./Kapal-Lawd.xcodeproj -scheme Kapal-Lawd -allowProvisioningUpdates
