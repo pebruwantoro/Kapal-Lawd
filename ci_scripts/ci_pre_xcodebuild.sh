c36ed53689fd80fe931f8fcff3fd6ad354547cf2
pebruwantoro <pebruwantoro.doni@gmail.com>
2024-09-30 21:08:13 +0700

    add-scripts

--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+echo "Current Directory: $(pwd)"
+ls -la
+
 # Check if the project was generated successfully
 if [ ! -d "Kapal-Lawd.xcodeproj" ]; then
     echo "Failed to generate Xcode project."
