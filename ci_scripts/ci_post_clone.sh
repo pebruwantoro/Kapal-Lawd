c2a29930d0eb5db15e598396de4c6a217caf64de
pebruwantoro <pebruwantoro.doni@gmail.com>
2024-09-30 19:34:14 +0700

    add-step-on-xcode-cloud

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,24 +1,81 @@
+# #!/bin/sh
+
+# # Install Xcodegen
+# if ! command -v xcodegen &> /dev/null
+# then
+#     brew install xcodegen
+# fi
+
+# echo "Current Directory: $(pwd)"
+# ls -la
+
+# # Go to Project Directory
+# cd ..
+
+# echo "Current Directory: $(pwd)"
+# ls -la
+
+# # Generate the Xcode project
+# xcodegen generate
+
+# # See all project
+# echo "Current Directory: $(pwd)"
+# ls -la
+
+# # Check if the project was generated successfully
+# if [ ! -d "Kapal-Lawd.xcodeproj" ]; then
+#     echo "Failed to generate Xcode project."
+#     exit 1
+# fi
+
+# echo "Current Directory: $(pwd)"
+# ls -la
+
+# ls -la Kapal-Lawd.xcodeproj
+# ls -la Kapal-Lawd.xcodeproj/project.xcworkspace
+# ls -la Kapal-Lawd.xcodeproj/project.xcworkspace/xcshareddata
+# ls -la Kapal-Lawd.xcodeproj/project.xcworkspace/xcshareddata/swiftpm
+
+# # Resolve package dependencies
+# xcodebuild -resolvePackageDependencies -project ./Kapal-Lawd.xcodeproj -scheme Kapal-Lawd -allowProvisioningUpdates
+
+# # Build the project
+# xcodebuild -project ./Kapal-Lawd.xcodeproj -scheme Kapal-Lawd -verbose
+
+# # Confirm resolution status
+# if [ $? -ne 0 ]; then
+#     echo "Failed to resolve package dependencies."
+#     exit 1
+# fi
+
 #!/bin/sh
 
 # Install Xcodegen
-if ! command -v xcodegen &> /dev/null
-then
+if ! command -v xcodegen &> /dev/null; then
     brew install xcodegen
+fi
+
+# Check if xcodebuild is available
+if ! command -v xcodebuild &> /dev/null; then
+    echo "xcodebuild is not installed. Please install Xcode command line tools."
+    exit 1
 fi
 
 echo "Current Directory: $(pwd)"
 ls -la
 
 # Go to Project Directory
-cd ..
+cd .. || { echo "Failed to change directory."; exit 1; }
 
 echo "Current Directory: $(pwd)"
 ls -la
 
 # Generate the Xcode project
-xcodegen generate
+if ! xcodegen generate; then
+    echo "Failed to generate Xcode project."
+    exit 1
+fi
 
-# See all project
 echo "Current Directory: $(pwd)"
 ls -la
 
@@ -37,13 +94,15 @@ ls -la Kapal-Lawd.xcodeproj/project.xcworkspace/xcshareddata
 ls -la Kapal-Lawd.xcodeproj/project.xcworkspace/xcshareddata/swiftpm
 
 # Resolve package dependencies
-xcodebuild -resolvePackageDependencies -project ./Kapal-Lawd.xcodeproj -scheme Kapal-Lawd -allowProvisioningUpdates
-
-# Build the project
-xcodebuild -project ./Kapal-Lawd.xcodeproj -scheme Kapal-Lawd -verbose
-
-# Confirm resolution status
-if [ $? -ne 0 ]; then
+if ! xcodebuild -resolvePackageDependencies -project ./Kapal-Lawd.xcodeproj -scheme Kapal-Lawd -allowProvisioningUpdates; then
     echo "Failed to resolve package dependencies."
     exit 1
 fi
+
+# Build the project
+if ! xcodebuild -project ./Kapal-Lawd.xcodeproj -scheme Kapal-Lawd -verbose; then
+    echo "Failed to build the project."
+    exit 1
+fi
+
+echo "Project build completed successfully."
