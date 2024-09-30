0653f4d1409259e6e216f2ab4165001da5c7a103
pebruwantoro <pebruwantoro.doni@gmail.com>
2024-09-30 17:05:31 +0700

    add-step-on-xcode-cloud

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 
+# Install Xcodegen
 brew install xcodegen
 
+# Go to Project Directory
 cd ..
 
+# Generate the Xcode project
 xcodegen generate
 
+# Resolve Swift package dependencies and create the Package.resolved file
 xcodebuild -resolvePackageDependencies -project Kapal-Lawd.xcodeproj
+
+# Ensure package dependencies are correctly set
+xcodebuild build -project Kapal-Lawd.xcodeproj -scheme Kapal-Lawd
