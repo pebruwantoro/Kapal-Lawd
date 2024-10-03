96d6a802931a3fd49d67f74fc2f7023af77ccdda
Doni Pebruwatoro <85363247+pebruwantoro@users.noreply.github.com>
2024-10-03 14:20:22 +0700

    Fix/app version (#11)
    * fixing-tag-version
    
    * update
    
    * update
    
    * update

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -11,16 +11,36 @@ ls .
 # Change to the project directory
 cd ..
 
-# Fetch the latest Git tag
-LATEST_TAG=$(git describe --tags $(git rev-list --tags --max-count=1))
+# Fetch the latest Git tag (should be a semantic version like 1.0.0)
+git fetch --tags
+GIT_TAG=$(git describe --tags $(git rev-list --tags --max-count=1))
+echo "GIT_TAG: $GIT_TAG"
 
-# If no tag found, default tag
-if [ -z "$LATEST_TAG" ]; then
-  LATEST_TAG="alpha-1.0.0"
+# Extract the numeric part of the tag (for MARKETING_VERSION, should be numeric like 1.0.0)
+VERSION=$(echo $GIT_TAG | grep -oE '[0-9]+(\.[0-9]+)*')
+
+# If no valid tag is found, default to 1.0.0
+if [ -z "$VERSION" ]; then
+  VERSION="1.0.0"
 fi
+echo "VERSION: $VERSION"
 
-# Export the tag so it can be used in the Xcodegen YAML file
-export CURRENT_PROJECT_VERSION=$LATEST_TAG
+# If the tag contains non-numeric parts (e.g., alpha-1.1.2), use the commit count as the build number
+if [[ $GIT_TAG =~ [^0-9.] ]]; then
+  # Set CURRENT_PROJECT_VERSION as the number of commits
+  BUILD_NUMBER=$(git rev-list --count HEAD)
+else
+  # If the tag is purely numeric, use the tag as the CURRENT_PROJECT_VERSION
+  BUILD_NUMBER=$VERSION
+fi
+echo "BUILD_NUMBER: $BUILD_NUMBER"
+
+# Export variables to be used in xcodegen
+export MARKETING_VERSION=$VERSION
+export CURRENT_PROJECT_VERSION=$BUILD_NUMBER
+
+echo "MARKETING_VERSION: $MARKETING_VERSION"
+echo "CURRENT_PROJECT_VERSION: $CURRENT_PROJECT_VERSION"
 
 # Generate the Xcode project using XcodeGen
 echo "Generating Xcode project..."
