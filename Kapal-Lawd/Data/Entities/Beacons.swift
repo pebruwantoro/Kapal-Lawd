56447b13fa3c4424d5bb1cac877ed0f73c7d7851
Romi Fadhurrohman Nabil <70937653+Ragnarom-kun@users.noreply.github.com>
2024-10-30 12:43:55 +0700

    feat: change directory name to entities & add rssi value on Beacons model

--- Kapal-Lawd/Data/Entities/Beacons.swift
+++ Kapal-Lawd/Data/Entities/Beacons.swift
@@ -10,9 +10,13 @@ import Foundation
 struct Beacons: Codable {
     let uuid: String   
     let backgroundSound: String
+    let minRssi: Float
+    let maxRssi: Float
     
     enum CodingKeys: String, CodingKey {
         case uuid
         case backgroundSound = "background_sound"
+        case minRssi
+        case maxRssi
     }
 }
