9c6d432b68de82259f7baa92bccb3a233edc224f
Romi Fadhurrohman Nabil <70937653+Ragnarom-kun@users.noreply.github.com>
2024-11-19 11:22:37 +0700

    feat: add app clip to project

---
+++ Kapal-LawdClip/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Kapal-LawdClip
+//
+//  Created by Romi Fadhurohman Nabil on 19/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Kapal-LawdClip/Kapal_LawdClipApp.swift
@@ -0,0 +1,17 @@
+//
+//  Kapal_LawdClipApp.swift
+//  Kapal-LawdClip
+//
+//  Created by Romi Fadhurohman Nabil on 19/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Kapal_LawdClipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
