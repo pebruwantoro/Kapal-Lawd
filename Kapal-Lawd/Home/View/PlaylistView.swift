805f33ed46f3e2399920efd36a575fddc212784a
sabaach <safrieb3@gmail.com>
2024-11-21 17:58:28 +0700

    Fix Button Back

--- Kapal-Lawd/Home/View/PlaylistView.swift
+++ Kapal-Lawd/Home/View/PlaylistView.swift
@@ -139,10 +139,10 @@ struct PlaylistView: View {
                                                     UIApplication.shared.open(url)
                                                 }
                                             }) {
-                                                Image("instagramIcon")
+                                                Image(systemName: "network")
                                                     .resizable()
                                                     .scaledToFit()
-                                                    .frame(width: 48, height: 48)
+                                                    .frame(width: 29, height: 22)
                                             }
                                         }
                                         .frame(maxWidth: .infinity, alignment: .center)
@@ -201,13 +201,14 @@ struct PlaylistView: View {
                             if !self.list.isEmpty && !self.isBack {
                                 PlayerView(
                                     isPlaying: $playlistPlayerViewModel.playlistPlayerManager.isPlaying,
-                                    list: $list
+                                    list: $list,
+                                    isExploring: .constant(false), collections: $collections
                                 )
                                 .environmentObject(playlistPlayerViewModel)
                                 .environmentObject(audioPlayerViewModel)
                                 .environmentObject(backgroundPlayerViewModel)
                             }
-                        }.padding(.bottom, 16)
+                        }
                     }
                     
                 }
